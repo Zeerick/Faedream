9e047c6e431703629f2811a140fdae7326a2022b
WillowThunder44 <60935365+WillowThunder44@users.noreply.github.com>
2020-02-12 09:01:05 +0000

    Cara's work so far 12.2.20
    What this does is it has an item that rolls around and turns certain objects red upon hitting them. Currently working on getting the ball to have a limited number of hits. Was planning to have that ready for today, but its taking me slightly longer than expected

---
+++ Assets/roseCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roseCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("character"))
+        {
+            if (GetComponent<Renderer>().material.color == Color.white)
+            {
+                transform.GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+        transform.GetComponent<Renderer>().material.color = Color.red;
+    }
+}
