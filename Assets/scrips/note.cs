55b71751eac052ce379c02343eb4d10e4927c5e3
Chloe <cy214@sussex.ac.uk>
2020-02-24 12:51:30 +0000

    adding note functionality
    added a note element onto a plane

---
+++ Assets/scrips/note.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class note : MonoBehaviour
+{
+
+    public Image noteImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        noteImage.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (Input.GetKeyDown((KeyCode.E)) && noteImage.enabled==false && other.CompareTag("Player"))
+        {
+            ShowNoteImage();
+        } 
+        else if (Input.GetKeyDown(KeyCode.R) && noteImage.enabled && other.CompareTag("Player"))
+        {
+            HideNoteImage();
+        }
+    }
+
+    public void HideNoteImage()
+    {
+        noteImage.enabled = false;
+    }
+
+    public void ShowNoteImage()
+    {
+        noteImage.enabled = true;
+    }
+}
+
