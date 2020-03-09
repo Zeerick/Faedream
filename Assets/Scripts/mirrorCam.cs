5952f051a095df2cc0382a0244e32f2305d75a30
Zeerick <60880399+Zeerick@users.noreply.github.com>
2020-03-09 12:24:54 +0000

    Bedroom colliders & snowwhite mirror

---
+++ Assets/Scripts/mirrorCam.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mirrorCam : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject mirror;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Reflect(mainCamera.transform.position, mirror.transform.eulerAngles);
+    }
+}
